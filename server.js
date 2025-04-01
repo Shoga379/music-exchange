67257df6789c8ebd7e37057e56ce8419e35abf27
Shoga379 <at.perepelitsa@gmail.com>
2025-04-01 22:39:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
@@ -15,7 +16,7 @@ if (fs.existsSync(DATA_FILE)) {
 }
 
 // Middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
